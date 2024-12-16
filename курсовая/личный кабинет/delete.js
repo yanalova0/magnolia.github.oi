67ef771103560ec8c77b59ecd57be933791a7b6c
Яна Логинова <122596772+yanalova0@users.noreply.github.com>
2024-12-16 18:52:21 +0300

    Delete курсовая directory

--- курсовая/личный кабинет/delete.js
+++
@@ -1,4 +0,0 @@
-    function deleteRecord(button) {
-        const record = button.closest('.record');
-        record.remove(); // Удаляет выбранный блок
-    }
--- курсовая/услуги/form.js
+++
@@ -1,9 +0,0 @@
-// Функция для открытия формы
-function openForm() {
-    document.getElementById("popupForm").style.display = "flex";
-}
-
-// Функция для закрытия формы
-function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
-}
